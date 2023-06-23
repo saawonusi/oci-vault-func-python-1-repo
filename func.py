86791c6d7da7fe1e430522cf32e9ab58e52c9ba1
sawonusi <stephen.awonusi@technipfmc.com>
2023-06-23 07:55:57 +0000

    func.py

--- func.py
+++ func.py
@@ -4,18 +4,19 @@ import io
 import oci
 import base64
 
-# replace secret_id value below with the ocid of your secret
-secret_id = "ocid1.vaultsecret.oc1.uk-london-1.amaaaaaakujrcpiawx73hpzl5ijy4mocmtlrbjyywl3dqherp3mr3kp6p4vq"
+secret_name = "ipaas-client-secret-1"
+# replace vault_id value below with the ocid of your vault
+vault_id = "ocid1.vault.oc1.uk-london-1.d5sitxohaaa6k.abwgiljtbl5pupmmj3haubob3hb475qozryypozwtr7ukjw4wa46qvcmocya"
 
 # retrieve secret
-def get_secret(secret_id):
+def get_secret(secret_name,vault_id):
     # by default this will hit the auth service in the region the instance is running.
     signer = oci.auth.signers.get_resource_principals_signer()
     decrypted_secret_content = ""
     try:
         # get instance principal context
         secret_client = oci.secrets.SecretsClient({}, signer=signer)
-        secret_content = secret_client.get_secret_bundle(secret_id).data.secret_bundle_content.content.encode('utf-8')
+        secret_content = secret_client.get_secret_bundle_by_name(secret_name,vault_id).data.secret_bundle_content.content.encode('utf-8')
         # decode and decrypt the secret_content
         decrypted_secret_content = base64.b64decode(secret_content).decode('utf-8')
     except Exception as ex:
@@ -25,7 +26,7 @@ def get_secret(secret_id):
 
 def handler(ctx, data: io.BytesIO = None):
     logging.getLogger().info("entered get_secret handler")
-    ocivault_secret = get_secret(secret_id)
+    ocivault_secret = get_secret(secret_name,vault_id)
     print(format(ocivault_secret))
 
 """
