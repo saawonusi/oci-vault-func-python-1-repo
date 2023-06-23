dd9a187d66e25da6f0223c29637f00769c189beb
sawonusi <stephen.awonusi@technipfmc.com>
2023-06-23 08:43:08 +0000

    func.py update

--- func.py
+++ func.py
@@ -1,33 +1,35 @@
 # utility function to get secret from oci vault
-import logging
 import io
-import oci
+import json
+import logging
 import base64
-
-secret_name = "ipaas-client-secret-2"
-# replace vault_id value below with the ocid of your vault
-vault_id = "ocid1.vault.oc1.uk-london-1.d5sitxohaaa6k.abwgiljtbl5pupmmj3haubob3hb475qozryypozwtr7ukjw4wa46qvcmocya"
-
-# retrieve secret
-def get_secret(secret_name,vault_id):
-    # by default this will hit the auth service in the region the instance is running.
-    signer = oci.auth.signers.get_resource_principals_signer()
-    # decrypted_secret_content = ""
-    try:
-        # get instance principal context
-        secret_client = oci.secrets.SecretsClient({}, signer=signer)
-        secret_content = secret_client.get_secret_bundle_by_name(secret_name,vault_id).data.secret_bundle_content.content.encode('utf-8')
-        # decode and decrypt the secret_content
-        decrypted_secret_content = base64.b64decode(secret_content).decode('utf-8')
-    except Exception as ex:
-        logging.getLogger().error("get_secret: failed to get secret" + str(ex))
-        raise
-    return decrypted_secret_content
+import sys
+import oci
+from oci.key_management.models import DecryptDataDetails
+from fdk import response
+# if function fails with no module 'oci', add oci to the requirements.txt file to force docker to  add it
 
 def handler(ctx, data: io.BytesIO = None):
-    logging.getLogger().info("entered get_secret handler")
-    ocivault_secret = get_secret(secret_name,vault_id)
-    print(format(ocivault_secret))
+    apitoken_secret = ""
+    try:
+
+        #passing the signer uses what would have been in config as far as i can tell
+        # config = dict(ctx.Config())
+        signer = oci.auth.signers.get_resource_principals_signer()
+        secrets_client = oci.secrets.SecretsClient({}, signer=signer)
+        
+        apitoken_content = secrets_client.get_secret_bundle_by_name(secret_name="ipaas-client-secret-2",
+            vault_id="ocid1.vault.oc1.uk-london-1.d5sitxohaaa6k.abwgiljtbl5pupmmj3haubob3hb475qozryypozwtr7ukjw4wa46qvcmocya").data.secret_bundle_content.content.encode('utf-8')
+        
+        apitoken_secret = base64.b64decode(apitoken_content).decode("utf-8")
+        print("INFO: Decrypting ipaas-client-secret-2", apitoken_secret, flush=True)
+    except (Exception, ValueError) as ex:
+        print("ERROR: Decrypting ipaas-client-secret-2", ex, flush=True)
+
+    return response.Response(
+        ctx, response_data=json.dumps({"ipaas-client-secret-2": "{0}".format(apitoken_secret)}),
+        headers={"Content-Type": "application/json"}
+    )
 
 """
 import io
